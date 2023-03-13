0e76d9b788e636c157b7ee3e5fd7f631646a2e8d
Pere1J <pere1josep@gmail.com>
2023-03-13 10:33:23 +0100

    refactored controller and added book repository

--- src/main/java/com/myFirstApi/first/controllers/BookController.java
+++ src/main/java/com/myFirstApi/first/controllers/BookController.java
@@ -2,30 +2,20 @@ package com.myFirstApi.first.controllers;
 
 
 import com.myFirstApi.first.models.Book;
+import com.myFirstApi.first.repositories.BookRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/books")
 @CrossOrigin ("*")
 public class BookController {
 
-    List<Book> booksDB = new ArrayList<>(
-            //List.of(new Book("Emma"), new Book("Tintín"))
-            List.of(new Book("Emma",true,"Jane Austen", "https://kbimages1-a.akamaihd.net/1ea81b28-2fa1-4796-b9b0-710123b48227/353/569/90/False/emma-503.jpg"),
-                    new Book("Tintín", false, "Hergè", "https://papersgirona.com/wp-content/webpc-passthru.php?src=https://papersgirona.com/wp-content/uploads/2020/07/IMG_0022.jpg&nocache=1"),
-                    new Book("Conan", false, "Howard", "https://m.media-amazon.com/images/I/712C4wexOxL.jpg"),
-                    new Book("Drácula", true, "Bram Stoker", "https://imagessl6.casadellibro.com/a/l/t7/36/9788415618836.jpg "),
-                    new Book("Tarzán", false, "Edward Rice Burroughs", "https://pictures.abebooks.com/isbn/9780307131942-es.jpg"),
-                    new Book("Capital", false, "Karl Marx", "https://pictures.abebooks.com/inventory/md/md30822975656.jpg"),
-                    new Book("Aurora", true, "Sandra Moya", "https://f7099cdac3ad9a351ae3-74c3dcaca50fe6cfec786b085d2f6908.ssl.cf5.rackcdn.com/books/images/lantia_cover_622af53a3dd25_360.jpg"),
-                    new Book("Un amor", false, "Sara Mesa", "https://www.anagrama-ed.es/uploads/media/portadas/0001/25/4de1878d73446124ee5b4e6c48b6f0fee6d8115a.jpeg"),
-                    new Book("Ivanhoe", false, "Walter Scott", "https://images.cdn2.buscalibre.com/fit-in/360x360/b5/bc/b5bcde278d2b5a97eb4b492c3361abec.jpg")
-            )
-    );
+    private final BookRepository bookRepository = new BookRepository();
 
 
     @GetMapping("/check")
@@ -36,51 +26,33 @@ public class BookController {
     @GetMapping
     public List<Book> getAll() {
 
-        return this.booksDB;
+        return this.bookRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Book getId(@PathVariable UUID id) {
-//        for (Book item:this.booksDB) {
-//            if (item.getId().equals(id))
-//                return item;
-//
-//        }
-//        return null;
+        return this.bookRepository.findById(id);
 
-        var book = this.booksDB.stream()
-                .filter(item -> item.getId().equals(id))
-                .findFirst().get();
-        return book;
     }
 
     @PostMapping
     public Book create(@RequestBody Book book) {
-        this.booksDB.add(book);
-        return book;
+        return this.bookRepository.save(book);
     }
 
     @DeleteMapping("/{id}")
     public Book deleteById(@PathVariable UUID id) {
-        for (Book item : this.booksDB) {
-            if (item.getId().equals(id)) {
-                this.booksDB.remove(item);
-                return item;
-            }
-
-        } return null;
+        return this.bookRepository.deleteItem(id);
     }
     @PutMapping("/{id}")
     public Book updateById(@PathVariable UUID id, @RequestBody Book book){
-        for (Book item:this.booksDB
-             ) { if (item.getId().equals(id)){
-                 item.setTitle(book.getTitle());
-                 return item;
-        }
-            
-        }
-        return null;
+        return  this.bookRepository.updateItem(id, book);
+    }
 
+    @GetMapping ("search")
+    public List<Book> searchBy (@RequestParam("title") String title){
+
+        return this.bookRepository.searchItem(title);
     }
 
 }
---
+++ src/main/java/com/myFirstApi/first/repositories/BookRepository.java
@@ -0,0 +1,68 @@
+package com.myFirstApi.first.repositories;
+
+import com.myFirstApi.first.models.Book;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class BookRepository {
+    List<Book> booksDB = new ArrayList<>(
+            //List.of(new Book("Emma"), new Book("Tintín"))
+            List.of(new Book("Emma",true,"Jane Austen", "https://kbimages1-a.akamaihd.net/1ea81b28-2fa1-4796-b9b0-710123b48227/353/569/90/False/emma-503.jpg"),
+                    new Book("Tintín", false, "Hergè", "https://papersgirona.com/wp-content/webpc-passthru.php?src=https://papersgirona.com/wp-content/uploads/2020/07/IMG_0022.jpg&nocache=1"),
+                    new Book("Conan", false, "Howard", "https://m.media-amazon.com/images/I/712C4wexOxL.jpg"),
+                    new Book("Drácula", true, "Bram Stoker", "https://imagessl6.casadellibro.com/a/l/t7/36/9788415618836.jpg "),
+                    new Book("Tarzán", false, "Edward Rice Burroughs", "https://pictures.abebooks.com/isbn/9780307131942-es.jpg"),
+                    new Book("Capital", false, "Karl Marx", "https://pictures.abebooks.com/inventory/md/md30822975656.jpg"),
+                    new Book("Aurora", true, "Sandra Moya", "https://f7099cdac3ad9a351ae3-74c3dcaca50fe6cfec786b085d2f6908.ssl.cf5.rackcdn.com/books/images/lantia_cover_622af53a3dd25_360.jpg"),
+                    new Book("Un amor", false, "Sara Mesa", "https://www.anagrama-ed.es/uploads/media/portadas/0001/25/4de1878d73446124ee5b4e6c48b6f0fee6d8115a.jpeg"),
+                    new Book("Ivanhoe", false, "Walter Scott", "https://images.cdn2.buscalibre.com/fit-in/360x360/b5/bc/b5bcde278d2b5a97eb4b492c3361abec.jpg")
+            )
+    );
+
+    public List<Book> findAll() {
+        return this.booksDB;
+    }
+    public Book findById (UUID id){
+        return this.booksDB.stream()
+                .filter(item -> item.getId().equals(id))
+                .findFirst().get();
+
+    }
+    public Book save(Book book){
+        this.booksDB.add(book);
+        return book;
+    }
+     public Book deleteItem(UUID id){
+         for (Book item : this.booksDB) {
+             if (item.getId().equals(id)) {
+                 this.booksDB.remove(item);
+                 return item;
+             }
+
+         } return null;
+
+     }
+     public Book updateItem(UUID id, Book book){
+
+         for (Book item:this.booksDB
+         ) { if (item.getId().equals(id)){
+             item.setTitle(book.getTitle());
+             return item;
+         }
+
+         }
+         return null;
+     }
+     public List<Book> searchItem(String title){
+         var filteredBooks = this.booksDB.stream()
+                 .filter(item -> item.getTitle().toLowerCase().contains(title.toLowerCase()))
+                 .collect(Collectors.toList());
+         return filteredBooks;
+     }
+
+
+}
